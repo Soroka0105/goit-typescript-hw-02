11289f99ac8bc4bc2411f63e62620bebf4b55316
Soroka0105 <soroka0105@gmail.com>
2024-01-05 19:53:59 +0200

    1

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,13 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+enum day{
+  MONDAY,
+  TUESDAY
+}
+function isWeekend (weekday):boolean {
+if (weekday === day.MONDAY || weekday === day.MONDAY ) {
+  return true
+}
+return false
+}
--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,8 +1,15 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
+type page ={
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details?: {}
+}
 
-const page1 = {
+const page1:page = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,7 +20,7 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2:page = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
